fa179609ec719925fe34f0737335a263da616c9d
MannyRoth <98435897+MannyRoth@users.noreply.github.com>
2022-03-15 10:18:38 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Chess_in_python
+''' Chess made from scratch that will have A.I. component added later'''
---
+++ main.py
@@ -0,0 +1,48 @@
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 400
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+def load_images():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR","bp","wp","wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+    for piece in pieces:
+        IMAGES[pieces] = p.transform.scale(p.image.load("images/"+piece+".png"), (SQ_SIZE, SQ_SIZE))
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    gs.board
+    load_images()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.quit:
+                running = False
+        draw_game_state(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    draw_pieces(screen, gs)
+
+def draw_board(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c)%2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def draw_pieces(screen, board):
+    pass
+
+
+if __name__ == '__main__':
+    main()
